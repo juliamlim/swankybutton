49cf5b0025cdc15c211510f47c8c7c17826336f7
juliamlim <juliamlim@hotmail.com>
2017-05-02 16:14:11 -0400

    Added the button

--- swanky-button/swanky-button.js
+++ swanky-button/swanky-button.js
@@ -1,8 +1,11 @@
-﻿// Include this file to implement the ** SWANKY ** button on your page
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+Include this file to implement the ** SWANKY ** button on your page
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 window.onload = function () {
     "use strict";
-    
     var get_swank = document.querySelector('link[rel="import"]').import;
     var swank_temp = get_swank.querySelector('#swanky-button'); // Selects the template from the imported document
     var swank_prot = Object.create(HTMLElement.prototype); // Creates a new prototype from the template
@@ -11,17 +14,4 @@ window.onload = function () {
         root.appendChild(get_swank.importNode(swank_temp.content, true));
     }; // Function implements template everytime a **swanky-button** element appears in document
     var swank_butt = document.registerElement('swanky-button', {prototype: swank_prot}); // Creates the custom element
-
-    
-    /******************************
-        Append Clock to Element
-    ******************************/
-    
-    //insert the module functionality to the humber-clock element
-    var current = document.querySelector("humber-clock .time");
-    var days = document.querySelector("humber-clock .count");
-    
-    
-    time.getTime(current); // time functionality
-    time.countDown(days); // countdown functionality
 };
