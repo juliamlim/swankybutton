e2581046e2a657b2e1eb6feac1fdd5bab4c5a0c9
juliamlim <juliamlim@hotmail.com>
2017-05-02 15:53:05 -0400

    Creating the button js

---
+++ swanky-button/swanky-button.js
@@ -0,0 +1,27 @@
+﻿// Include this file to implement the ** SWANKY ** button on your page
+
+window.onload = function () {
+    "use strict";
+    
+    var get_swank = document.querySelector('link[rel="import"]').import;
+    var swank_temp = get_swank.querySelector('#swanky-button'); // Selects the template from the imported document
+    var swank_prot = Object.create(HTMLElement.prototype); // Creates a new prototype from the template
+    swank_prot.createdCallback = function() {
+        var root = this.attachShadow({mode: 'open'});
+        root.appendChild(get_swank.importNode(swank_temp.content, true));
+    }; // Function implements template everytime a **swanky-button** element appears in document
+    var swank_butt = document.registerElement('swanky-button', {prototype: swank_prot}); // Creates the custom element
+
+    
+    /******************************
+        Append Clock to Element
+    ******************************/
+    
+    //insert the module functionality to the humber-clock element
+    var current = document.querySelector("humber-clock .time");
+    var days = document.querySelector("humber-clock .count");
+    
+    
+    time.getTime(current); // time functionality
+    time.countDown(days); // countdown functionality
+};
